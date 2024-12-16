077cdfd613027ab269c1b8bbcc80854ddd04b126
hiernymus <hiernysident@outlook.com>
2024-12-17 00:41:33 +0800

    del extend EuclideanAlgorithm.py.Hasn't over

--- lib/EuclideanAlgorithm.py
+++ lib/EuclideanAlgorithm.py
@@ -1,10 +1,14 @@
+from queue import Full
 from sage.all import *
 from numpy import NaN
 
 
-def EuclideanAlgorithm(a,b):
+def EuclideanAlgorithm(a,b,_x=[1,0],_y=[0,1],_count=-1):
     m = 0
     n = 0
+    x = _x
+    y = _y
+    c = _count + 1
     if a>b:
         m = a
         n = b
@@ -12,8 +16,18 @@ def EuclideanAlgorithm(a,b):
         m = b
         n = a
     r = m % n
-    if r == 0:
-        return n
+    q = m // n
+    if c == 0:
+        x[1] = x[0] - q*x[1]
+        y[1] = y[0] - q*y[1]
     else:
-            return EuclideanAlgorithm(b,r)
+        x.append(-1)
+        y.append(-1)
+        x[c]=x[c-2] - q*x[c-1]
+        y[c]=y[c-2] - q*y[c-1]
+    if r == 0:
+        return (n,x,y)
+    else:
+        return EuclideanAlgorithm(b,r,x,y,c)
 
+show(EuclideanAlgorithm(120,94))
